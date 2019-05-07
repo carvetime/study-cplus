32c1829eb4cf373d15e62f226967ae80ebe90683
gaowenjie7 <yidong-ios16>
2019-05-07 11:34:29 +0800

    basic data type

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <string>
+#include <limits>
+
+using namespace std;
 
 int main(){
-	std::cout << "Hello world";
+	cout << "type \t\t" << "************size**********"<< endl;
+	cout << "bool \t\t" << "所占字节数:" << sizeof(bool);
+	cout << "\t最大值:" << (numeric_limits<bool>::max)();
+	cout << "\t最小值:" << (numeric_limits<bool>::min)() << endl;
+	cout << "type \t\t" << "************size**********"<< endl;
 	return 0;
 }
