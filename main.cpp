dbb47cad87ca9a38912884d6baa835214f687f26
gaowenjie7 <yidong-ios16>
2019-05-07 16:18:35 +0800

    lambda

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
-
-int count;
-extern void write_extern();
-int main(){
-	count = 5;
-	write_extern();
+// auto only suport c++11, so compile use: g++ main.cpp -std=c++11
+int main()
+{
+	auto increaceLb = [](int a,int b) -> int { return a + b; };
+	int a = increaceLb(10,10);
+	std::cout << "a is " << a ; 
 	return 0;
 }
