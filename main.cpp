919203b26e507abd7c82f7e61981e132cc5aa15b
gaowenjie7 <yidong-ios16>
2019-05-07 11:40:07 +0800

    enum define

--- main.cpp
+++ main.cpp
@@ -5,10 +5,10 @@
 using namespace std;
 
 int main(){
-	cout << "type \t\t" << "************size**********"<< endl;
-	cout << "bool \t\t" << "所占字节数:" << sizeof(bool);
-	cout << "\t最大值:" << (numeric_limits<bool>::max)();
-	cout << "\t最小值:" << (numeric_limits<bool>::min)() << endl;
-	cout << "type \t\t" << "************size**********"<< endl;
+	typedef int feet;
+	feet distance;
+	enum color {red, green, blue} c;
+	c = blue;
+	cout << "c type value:" << c;
 	return 0;
 }
