8ca33359115628d1cdda9d2288f34eafc6b32995
gaowenjie7 <yidong-ios16>
2019-05-08 20:06:14 +0800

    & and *

--- main.cpp
+++ main.cpp
@@ -1,22 +1,32 @@
 #include <iostream>
 
-using std::cout;
 
-namespace first_space{
-        void func(){
-                cout << "first space func" << std::endl;
-        }
-	namespace second_space{
-		void  func(){
-			cout << "second sapce func" << std::endl;
-		}
-	}
+void demo1(){
+	int a = 1;
+        int &ra = a;
+        //int &rb;  //error: vaule can't be empty
+        const int &rb = 10;
+	std::cout << "demo1" ;
 }
 
-//using namespace first_space;
-using namespace first_space::second_space;
+void demo2(){
+	//int& ra = 1; // error:non-const lvalue reference to type 'int' cannot bind to a temporary of type 'int' 
+}
+
+void demo3(){
+	int a = 1;
+	int b = 2;
+	int&ra = a;
+	//int&ra = b; //error: can't change value of target;
+}
+
+void demo4(){
+	int * p; // inital value can be empty;
+}
+
 int main()
 {
-        func();
+	
         return 0;
 }
+
